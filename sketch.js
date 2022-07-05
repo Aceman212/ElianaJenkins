9b88723b3a4360318478815a81d4a7944d17e5a1
Aceman212 <108763052+Aceman212@users.noreply.github.com>
2022-07-05 18:32:43 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+// $(document).ready(function() {
+//     $("#my_audio").get(0).play();
+// });
+
+function run(url){
+    document.getElementById("typewriter").style.visibility = "visible";
+    var snd = new Audio("ddancer.mp3");
+    snd.play()
+}
+
+
+$( "#header-plugin" ).load( "https://vivinantony.github.io/header-plugin/", function() {
+	$("a.back-to-link").attr("href", "http://blog.thelittletechie.com/2015/03/love-heart-animation-using-css3.html#tlt")  
+});
+
+var love = setInterval(function() {
+    var r_num = Math.floor(Math.random() * 40) + 1;
+    var r_size = Math.floor(Math.random() * 65) + 10;
+    var r_left = Math.floor(Math.random() * 100) + 1;
+    var r_time = Math.floor(Math.random() * 3) + 5;
+
+    $('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(69,24,98);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
+
+    $('.bg_heart').append("<div class='heart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(69,49,98,1);-webkit-animation:love " + (r_time + 5) + "s ease;-moz-animation:love " + (r_time + 5) + "s ease;-ms-animation:love " + (r_time + 5) + "s ease;animation:love " + (r_time + 5) + "s ease'></div>");
+
+    $('.heart').each(function() {
+        var top = $(this).css("top").replace(/[^-\d\.]/g, '');
+        var width = $(this).css("width").replace(/[^-\d\.]/g, '');
+        if (top <= -100 || width >= 150) {
+            $(this).detach();
+        }
+    });
+}, 500);
+
+
+// function playSound(){
+//     var audio = new Audio('ddancer.mp3');
+//     audio.play();
+// }
+
+
+
+
